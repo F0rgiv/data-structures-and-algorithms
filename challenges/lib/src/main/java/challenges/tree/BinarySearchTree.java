ebcec06e3f1ddc617ee96cc6fe3ad8afb2ce9a91
James Mansour <jamestmansour@outlook.com>
2021-04-09 17:21:02 -0700

    Formatting

--- challenges/lib/src/main/java/challenges/tree/BinarySearchTree.java
+++ challenges/lib/src/main/java/challenges/tree/BinarySearchTree.java
@@ -1,7 +1,7 @@
 package challenges.tree;
 
-public class BinarySearchTree<T extends Comparable<T>> extends Tree<T>{
-    public void add(T value){
+public class BinarySearchTree<T extends Comparable<T>> extends Tree<T> {
+    public void add(T value) {
         if (this.root == null) {
             this.root = new Node<>(value);
             return;
@@ -9,28 +9,27 @@ public class BinarySearchTree<T extends Comparable<T>> extends Tree<T>{
         add(value, this.root);
     }
 
-    private void add(T value, Node<T> node){
-        if (node.value.compareTo(value) < 0){
-            if (node.right == null){
+    private void add(T value, Node<T> node) {
+        if (node.value.compareTo(value) < 0) {
+            if (node.right == null) {
                 node.right = new Node<>(value);
                 return;
             }
             add(value, node.right);
             return;
         }
-        if (node.left == null){
+        if (node.left == null) {
             node.left = new Node<>(value);
             return;
         }
         add(value, node.left);
-        return;
     }
 
-    public boolean contains(T value){
+    public boolean contains(T value) {
         Node<T> node = this.root;
-        while(node != null){
-            if(node.value == value) return true;
-            if(node.value.compareTo(value) < 0){
+        while (node != null) {
+            if (node.value == value) return true;
+            if (node.value.compareTo(value) < 0) {
                 node = node.right;
                 continue;
             }
