82e22e6246bf9ba9754054ed4179359bfec10a98
James Mansour <jamestmansour@outlook.com>
2021-05-04 14:39:00 -0700

    Adds readme and refactors to use a Hashmap instead of Binary search tree for the contains check.

--- challenges/lib/src/main/java/challenges/treeintersection/TreeIntersection.java
+++ challenges/lib/src/main/java/challenges/treeintersection/TreeIntersection.java
@@ -5,12 +5,14 @@ import challenges.tree.Tree;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class TreeIntersection {
     public static<T extends Comparable<T>> Set<T> treeIntersection(BinarySearchTree<T> tree1, BinarySearchTree<T> tree2){
         Set<T> result = new HashSet<>();
+        Set<T> tree2Vals = new HashSet<>(tree2.breadthTraversal());
         tree1.breadthTraversal().forEach(val -> {
-            if (tree2.contains(val)) result.add(val);
+            if (tree2Vals.contains(val)) result.add(val);
         });
         return result;
     }
