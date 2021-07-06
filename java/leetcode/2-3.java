7393bb70e8e191ce05f08e928b7d297d64b10fe4
James Mansour <jamestmansour@outlook.com>
2021-07-06 11:11:01 -0700

    resolves question 9

--- java/leetcode/2-3.java
+++ java/leetcode/2-3.java
@@ -326,3 +326,69 @@ class Solution {
         return result;
     }
 }
+
+/**
+ * https://leetcode.com/problems/palindrome-number/
+ * An integer is a palindrome when it reads the same backward as forward. For example, 121 is palindrome while 123 is not.
+ * 121 -> true
+ * 1001 -> true
+ * 1101 -> false
+ * -12321 -> false
+ * 12321 -> true
+ * 100021 -> false
+ */
+class Solution {
+    public boolean isPalindrome(int x) {
+        //catch negative nums
+        if (x < 0) return false; 
+        
+        //get count of ints
+        int length = 1;
+        int len = x;
+        while (len > 9){
+            len = len / 10;
+            length++;
+        }
+        
+        while (length > 1){            
+            //pull
+            int y = x;
+            for (int i = 1; i<length; i++){
+                y = y / 10;
+            }
+            int first = y % 10;
+            length -= 2;
+
+            //pop
+            int last = x % 10;
+            x = x / 10;
+            
+            //compare
+            if (first != last) return false;
+        }
+        return true;
+    }
+
+    public boolean isPalindrome2(int x) {
+        // Special cases:
+        // As discussed above, when x < 0, x is not a palindrome.
+        // Also if the last digit of the number is 0, in order to be a palindrome,
+        // the first digit of the number also needs to be 0.
+        // Only 0 satisfy this property.
+        if(x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int revertedNumber = 0;
+        while(x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
+        }
+
+        // When the length is an odd number, we can get rid of the middle digit by revertedNumber/10
+        // For example when the input is 12321, at the end of the while loop we get x = 12, revertedNumber = 123,
+        // since the middle digit doesn't matter in palidrome(it will always equal to itself), we can simply get rid of it.
+        return x == revertedNumber || x == revertedNumber/10;
+    }
+    }
+}
